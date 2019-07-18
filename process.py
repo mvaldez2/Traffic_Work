fc533834cf9776b0c28d44dedfba02076f33358b
mvaldez2 <38698523+mvaldez2@users.noreply.github.com>
2019-07-18 14:11:25 -0700

    Add files via upload

--- process.py
+++ process.py
@@ -16,16 +16,22 @@ path = 'T:/SR2/logs/processed2'
 extension = 'csv'
 os.chdir(path) 
 
+current_day = datetime.now().day
 current_month = datetime.now().month
+current_year = datetime.now().year
+
+import datetime
+current_week = datetime.date(current_year, current_month, current_day).isocalendar()[1]
 
 #looks for data from the current month to process
 for infile in glob.glob('*.{}'.format(extension)):
-    month = (re.findall('\d+', infile))
-    month = int(month[1])
-    if month >= current_month:
-        command = ('python T:/Programs/Miguel/Done/combined_scripts_dir.py T:/Programs/Miguel/test2/'+infile+' done')
+    date = (re.findall('\d+', infile))
+    week = datetime.date(int(date[0]), int(date[1]), int(date[2])).isocalendar()[1]
+    if week >= current_week:
+        command = ('python T:/Programs/Miguel/Done/combined_scripts_dir.py T:/SR2/logs/processed2/'+infile+' done')
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, shell=True)
         output, error = process.communicate()
+        print("Included: " + infile)
     else:
-        print("Not included")
+        print("Not included: " + infile)
     
