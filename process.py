7f2d593ca2482f97c5d5f616d26c57085750552c
mvaldez2 <38698523+mvaldez2@users.noreply.github.com>
2019-07-18 13:31:25 -0700

    Add files via upload

--- process.py
+++ process.py
@@ -9,16 +9,21 @@ import os
 import glob
 import subprocess
 import re
+from datetime import datetime
+
 
 path = 'T:/SR2/logs/processed2'
 extension = 'csv'
 os.chdir(path) 
 
+current_month = datetime.now().month
+
+#looks for data from the current month to process
 for infile in glob.glob('*.{}'.format(extension)):
     month = (re.findall('\d+', infile))
     month = int(month[1])
-    if month >= 7:
-        command = ('python T:/Programs/Miguel/test2/combined_scripts_dir.py T:/Programs/Miguel/test2/'+infile+' done')
+    if month >= current_month:
+        command = ('python T:/Programs/Miguel/Done/combined_scripts_dir.py T:/Programs/Miguel/test2/'+infile+' done')
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, shell=True)
         output, error = process.communicate()
     else:
